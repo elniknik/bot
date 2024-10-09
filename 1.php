017edec18233c1a3023ce820dbb7c2591c5fa7bd
elniknik <elniknik02@gmail.com>
2024-10-09 20:51:35 +0300

    1

---
+++ 1.php
@@ -0,0 +1,60 @@
+<?php
+
+/*
+
+Classes:
+1. Описують сутність у загальному вигляді
+2. Повинен описувати одну і тільки одну сутність
+*/
+
+class User {
+    private $firstname;
+    private $dateBirth;
+    private $email;
+    private $password;
+    private $info;
+
+    public function __construct($user)
+    {
+        $this->firstname = $user["firstname"];
+        $this->dateBirth = $user["dateBirth"];
+        $this->email = $user["email"];
+        $this->password = $user["password"];
+        $this->info = $user["info"];
+    }
+
+    public function isAuth($email, $password) {
+        return $email === $this->email && $password === $this->password;
+    }
+
+    public function getInfo() {
+        return $this->info;
+    }
+}
+
+if(!isset($_GET["email"]) || !isset($_GET["password"])) {
+    exit();
+}
+
+$users = [
+    new User([
+        "firstname" => "Alex",
+        "dateBirth" => "1995-01-02",
+        "password" => "qwerty",
+        "email" => "alex@email.com",
+        "info" => "1111111111"
+    ]),
+    new User([
+        "firstname" => "Alex",
+        "dateBirth" => "1995-01-02",
+        "password" => "ytrew",
+        "email" => "alex@email.com",
+        "info" => "22222"
+    ]),
+];
+
+for($i = 0, $count = count($users); $i < $count; $i++) {
+    if($users[$i]->isAuth()) {
+        echo $users[$i]->getInfo();
+    }
+}
---
+++ 2.php
@@ -0,0 +1,64 @@
+<?php
+
+/*
+
+Classes:
+1. Описують сутність у загальному вигляді
+2. Повинен описувати одну і тільки одну сутність
+3. Якщо за допомогою класс уне можемо створити корректний об'єкт - то це по рушує умови классу
+
+Об'єкт:
+1. Повинен логічно існувати у реальному житі
+2.
+
+Конструктор:
+1. Конструктор - створює об'єкт
+2. Ціль: Створити корректний об'єкт
+*/
+
+abstract class Shape {
+    public abstract function square();
+}
+
+class Rectangle extends Shape {
+    private $width;
+    private $height;
+
+    public function __construct($width, $height)
+    {
+        if((int)$width <= 0 || (int)$height <= 0) {
+            throw new Exception("Fail");
+        }
+
+        $this->width = $width;
+        $this->height = $height;
+    }
+
+    public function square() {
+        return $this->width * $this->height;
+    }
+}
+
+class Circle extends Shape {
+    private $radius;
+
+    public function __construct($radius)
+    {
+        $this->radius = $radius;
+    }
+
+    public function square() {
+        return M_PI * ($this->radius * $this->radius);
+    }
+}
+
+function getMinFigure(Shape $shape1, Shape $shape2) {
+    if($shape1->f() < $shape2->square()) {
+        return $shape1;
+    }
+
+    return $shape2;
+}
+
+$shape = getMinFigure(new Circle(10), new Rectangle(2, 4));
+var_dump($shape);
